760c7164c07e8eaa712b55d7bb1ee656712a4cf1
Nieruuu <rafly23yoi@gmail.com>
2026-03-27 15:12:19 +0700

    ngubah navbar lagi

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
                         class="inline-flex items-center gap-3 rounded-full border border-white/10 bg-white/5 px-3 py-2 text-white">
                         <span
                             class="grid size-8 place-items-center rounded-full bg-amber-400 text-sm font-black text-slate-900">
-                            W
+                            R
                         </span>
                         <span class="text-sm font-semibold tracking-[0.24em] uppercase">Raflyblog</span>
                     </a>
